0dc11ce1dbbcd607193c0dd0b10859c9d790a252
Kranthi Guribilli <79071810+Kranthi-Guribilli@users.noreply.github.com>
2021-12-11 19:37:42 +0530

    Update Sudoku|Backtracking.cpp

--- Sudoku|Backtracking.cpp
+++ Sudoku|Backtracking.cpp
@@ -111,11 +111,13 @@ bool isSafe(int grid[N][N], int row,
 /* A utility function to print grid */
 void printGrid(int grid[N][N])
 {
+	cout << "+-----+-----+-----+\n";
 	for (int row = 0; row < N; row++)
 	{
 		for (int col = 0; col < N; col++)
-			cout << grid[row][col] << " ";
-		cout << endl;
+			cout <<"|" << grid[row][col] ;
+		cout << "|\n";
+		if((row+1)%3==0)cout << "+-----+-----+-----+\n";
 	}
 }
 
